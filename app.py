d4b9458861225ec81f86e8ab4c8aa49e0b140cc3
Sibgat-git <sibgatullahkhanzehady@gmail.com>
2025-12-18 10:27:18 +0600

    worked on priliminary version of website

--- app.py
+++ app.py
@@ -1,4 +1,15 @@
-from flask import Flask
+from flask import Flask,render_template
 import similarity_check as sc
+import os
 
 
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    check = sc.similarity_check("food","Food")
+    return render_template('home.html', check = check)
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT',5000))
+    app.run(debug=True,host='0.0.0.0',port = port)
