1ce1db48f1f404c0bb7bd73dd807189dedf22be0
Sibgat-git <sibgatullahkhanzehady@gmail.com>
2025-12-19 21:34:47 +0600

    preliminary version done this can now handle text

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request,url_for
 import similarity_check as sc
 import os
 
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    check = sc.similarity_check("food","Food")
-    return render_template('home.html', check = check)
+
+
+@app.route('/' , methods = ["GET","POST"])
+def text():
+
+    score = None
+
+    if request.method == "POST":
+        text1 = request.form.get('text1')
+        text2 = request.form.get('text2')
+
+        score = sc.similarity_check(text1,text2)
+    return render_template('text.html', score = score)
+
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT',5000))
